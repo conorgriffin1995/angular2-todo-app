7216acfacc3302c5685bed4b4b4de37abe8ee6e7
Conor Griffin <conorgriffin@Conors-MacBook-Pro.local>
2016-11-16 16:28:27 +0000

    Web App Development Todo App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Angular2TodoApp', () => {
+describe('App: TodoApp', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
